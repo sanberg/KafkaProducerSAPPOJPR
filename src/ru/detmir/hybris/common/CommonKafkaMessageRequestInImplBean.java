3ba916eafeb927ee5ee107309b97b95573bee4dd
sanberg <eugenesanberg@gmail.com>
2023-02-03 16:20:40 +0300

    update logging

--- src/ru/detmir/hybris/common/CommonKafkaMessageRequestInImplBean.java
+++ src/ru/detmir/hybris/common/CommonKafkaMessageRequestInImplBean.java
@@ -12,6 +12,8 @@ import com.sap.engine.services.webservices.espbase.configuration.ann.dt.Authenti
 import com.sap.engine.services.webservices.espbase.configuration.ann.dt.AuthenticationEnumsAuthenticationLevel;
 import com.sap.engine.services.webservices.espbase.configuration.ann.dt.TransportGuaranteeDT;
 import com.sap.engine.services.webservices.espbase.configuration.ann.dt.TransportGuaranteeEnumsLevel;
+import com.sap.engine.services.webservices.espbase.configuration.ann.dt.XIEnabled;
+import com.sap.engine.services.webservices.espbase.configuration.ann.rt.TransportBindingRT;
 import com.sap.engine.services.webservices.espbase.server.additions.xi.ProviderXIMessageContext;
 import com.sap.tc.logging.Location;
 
@@ -27,7 +29,7 @@ import javax.ejb.TransactionAttribute;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.clients.producer.ProducerRecord;
-import org.apache.kafka.common.serialization.StringSerializer;
+import org.apache.kafka.clients.producer.RecordMetadata;
 
 @SessionHandlingDT(enableSession = false)
 @Addressing(enabled = true)
@@ -35,7 +37,19 @@ import org.apache.kafka.common.serialization.StringSerializer;
 @AuthenticationDT(authenticationLevel = AuthenticationEnumsAuthenticationLevel.BASIC)
 @WebService(portName = "CommonKafkaMessageRequest_In_Port", serviceName = "CommonKafkaMessageRequest_In_Service", endpointInterface = "ru.detmir.hybris.common.CommonKafkaMessageRequestIn", targetNamespace = "urn:DetMir.ru:Hybris:Common", wsdlLocation = "META-INF/wsdl/ru/detmir/hybris/common/CommonKafkaMessageRequest_In/CommonKafkaMessageRequest_In.wsdl")
 @Stateless
+@XIEnabled
+@TransportBindingRT(AltPath = "{urn:DetMir.ru:Hybris:Common}CommonKafkaMessageRequest_In")
 public class CommonKafkaMessageRequestInImplBean {
+
+    private static void addAuditLogEntry(AuditLogStatus status, String s, MessageMonitor monitor) throws RuntimeException {
+        try {
+            monitor.addLogEntry(status, s);
+        } catch (NoSuchMethodException | NoSuchFieldException | IllegalAccessException | InvocationTargetException e) {
+            e.printStackTrace();
+            throw new RuntimeException();
+        }
+    }
+
     @TransactionAttribute()
     @RelMessagingNW05DTOperation(enableWSRM = true)
     public void commonKafkaMessageRequestIn(CommonKafkaMessageRequest commonKafkaMessageRequest) {
@@ -50,32 +64,35 @@ public class CommonKafkaMessageRequestInImplBean {
         } catch (NamingException | InstantiationException | IllegalAccessException | NoSuchMethodException |
                  InvocationTargetException | NoSuchFieldException e) {
             LOG.debugT("CommonKafkaMessageRequest_In_Proxy: AuditLog object isn't assigned. Exception occurred: " + e.getMessage());
-            throw new RuntimeException();
+            throw new RuntimeException("CommonKafkaMessageRequest_In_Proxy: AuditLog object isn't assigned. Exception occurred: " + e.getMessage());
         }
         addAuditLogEntry(AuditLogStatus.SUCCESS, "CommonKafkaMessageRequest_In_Proxy: Processing started", monitor);
+
         String server = commonKafkaMessageRequest.getServer();
-
-        Properties properties = new Properties();
-        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, server);
-        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
-        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
-
+        Properties properties = new java.util.Properties();
+        properties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, server);
+        addAuditLogEntry(AuditLogStatus.SUCCESS, "CommonKafkaMessageRequest_In_Proxy: server is: " + server, monitor);
+        properties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer");
+        properties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer");
         KafkaProducer<String, String> producer = new KafkaProducer<>(properties);
-
-        String decodedData = new String(Base64.getDecoder().decode(commonKafkaMessageRequest.getData()));
-        ProducerRecord<String, String> producerRecord = new ProducerRecord<>(commonKafkaMessageRequest.getTopicName(), decodedData);
-        producer.send(producerRecord);
-        producer.flush();
-        producer.close();
-    }
-
-    private static void addAuditLogEntry(AuditLogStatus status, String s, MessageMonitor monitor) throws RuntimeException {
         try {
-            monitor.addLogEntry(status, s);
-        } catch (NoSuchMethodException | NoSuchFieldException | IllegalAccessException | InvocationTargetException e) {
-            e.printStackTrace();
-            throw new RuntimeException();
+            String decodedData = new String(Base64.getDecoder().decode(commonKafkaMessageRequest.getData()));
+            ProducerRecord<String, String> producerRecord = new ProducerRecord<>(commonKafkaMessageRequest.getTopicName(), decodedData);
+            RecordMetadata metadata = producer.send(producerRecord).get();
+            addAuditLogEntry(AuditLogStatus.SUCCESS, "CommonKafkaMessageRequest_In_Proxy: message was sent to topic "
+                    + commonKafkaMessageRequest.getTopicName() + "with " +
+                    "offset: " + metadata.offset() +
+                    "timestamp: " + metadata.timestamp(), monitor);
+        } catch (Exception e) {
+            Throwable t = e;
+            while (t.getCause() != null) {
+                addAuditLogEntry(AuditLogStatus.ERROR, "CommonKafkaMessageRequest_In_Proxy: " + t.getCause(), monitor);
+                t = t.getCause();
+            }
+            throw new RuntimeException(e.getMessage());
+        } finally {
+            producer.close();
+            addAuditLogEntry(AuditLogStatus.SUCCESS, "CommonKafkaMessageRequest_In_Proxy: producer closed", monitor);
         }
     }
-
 }
